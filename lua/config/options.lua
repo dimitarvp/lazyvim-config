2c51f6b1db4b54d7106610d3856639e4cb1aabb8
Dimitar Panayotov <mitko.p@gmail.com>
2026-09-02 20:21:00 +0300

    options: disable unused remote-plugin providers

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -9,6 +9,12 @@ vim.o.autoread = true
 vim.o.confirm = false
 vim.g.snacks_animate = false
 
+-- No plugin here needs the remote-plugin providers; the interpreters they
+-- would probe (pynvim, ruby, perl) are not installed on purpose
+vim.g.loaded_python3_provider = 0
+vim.g.loaded_ruby_provider = 0
+vim.g.loaded_perl_provider = 0
+
 -- Docker-compose filetype detection
 vim.filetype.add({
   filename = {
